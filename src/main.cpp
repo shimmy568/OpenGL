68e127d62c99f1a2c3c4f1470c04052c74949b6f
Owen Anderson <owenthomasanderson@gmail.com>
2017-12-10 20:39:14 -0500

    TRIANGLE RAVE

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
+#include <math.h>
+#include <chrono>
 
 int main()
 {
@@ -76,6 +78,9 @@ int main()
     glLinkProgram(shaderProgram);
     glUseProgram(shaderProgram);
 
+    //Set color via uniform
+    GLint uniColor = glGetUniformLocation(shaderProgram, "triangleColor");
+
     //Vertex array object (look into how these work)
     GLuint vao;
     glGenVertexArrays(1, &vao);
@@ -85,9 +90,17 @@ int main()
     GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
     glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
     glEnableVertexAttribArray(posAttrib);
+    auto t_start = std::chrono::high_resolution_clock::now();
 
     while (!glfwWindowShouldClose(window))
     {
+        //Change triangle color with time
+
+        auto t_now = std::chrono::high_resolution_clock::now();
+        float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+        glUniform3f(uniColor, (sin(time * 3.0f) + 1.0f) / 2.0f, (sin(time * 2.0f) + 1.0f) / 2.0f, (sin(time * 5.0f) + 1.0f) / 2.0f);
+        
+
         //Shit go here
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
